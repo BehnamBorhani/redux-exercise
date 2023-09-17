7e6393e2f06c136b65266549f806e574c7f92757
Behnam <behnamborhani95@gmail.com>
2023-09-17 20:36:25 +0330

    add remove student btn

--- src/components/Students/Students.js
+++ src/components/Students/Students.js
@@ -1,27 +1,54 @@
 import * as React from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import { useGetStudentsQuery } from "../../redux/services/studentsApi";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import EditIcon from "@mui/icons-material/Edit";
+import {
+  useAddStudentMutation,
+  useGetStudentsQuery,
+  useRemoveStudentMutation,
+} from "../../redux/services/studentsApi";
+import "./Students.scss";
 
 export default function Students() {
   const { data: rows, isLoading } = useGetStudentsQuery();
+  const [addStudent, { isLoading: isAddingStudent }] = useAddStudentMutation();
+  const [removeStudent, { isLoading: isRemovingStudent }] =
+    useRemoveStudentMutation();
+
   const columns = [
-   { field: "id", headerName: "ID", width: 70 },
-   { field: "name", headerName: "نام", width: 130 },
-   { field: "family", headerName: "نام خانوادگی", width: 130 },
-   {
-     field: "fieldOfStudy",
-     headerName: "رشته تحصیلی",
-     width: 200,
-   },
-   {
-     field: "studentNumber",
-     headerName: "شماره دانشجویی",
-     width: 200,
-   },
- ];
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "name", headerName: "نام", width: 130 },
+    { field: "family", headerName: "نام خانوادگی", width: 130 },
+    {
+      field: "fieldOfStudy",
+      headerName: "رشته تحصیلی",
+      width: 200,
+    },
+    {
+      field: "studentNumber",
+      headerName: "شماره دانشجویی",
+      width: 200,
+    },
+    {
+      field: "delete",
+      headerName: "حذف",
+      width: 50,
+      renderCell: (params) => {
+        return (
+          <>
+            <DeleteOutlineIcon
+              className="actionIcons studentDeleteIcon"
+              onClick={() => removeStudent(params.row.id)}
+            />
+          </>
+        );
+      },
+    },
+  ];
 
   return (
     <section id="students">
+      <h1>لیست دانشجویان</h1>
       {rows?.length && (
         <DataGrid
           rows={rows}
