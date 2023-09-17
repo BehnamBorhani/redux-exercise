6e0c828b0213f7a31703b9095ca532d9070592a6
Behnam <behnamborhani95@gmail.com>
2023-09-17 19:29:48 +0330

    show all students

--- src/components/Students/Students.js
+++ src/components/Students/Students.js
@@ -1,10 +1,40 @@
-import React from 'react'
-import "./Students.scss"
+import * as React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { useGetStudentsQuery } from "../../redux/services/studentsApi";
 
-const Students = () => {
+export default function Students() {
+  const { data: rows, isLoading } = useGetStudentsQuery();
+  const columns = [
+   { field: "id", headerName: "ID", width: 70 },
+   { field: "name", headerName: "نام", width: 130 },
+   { field: "family", headerName: "نام خانوادگی", width: 130 },
+   {
+     field: "fieldOfStudy",
+     headerName: "رشته تحصیلی",
+     width: 200,
+   },
+   {
+     field: "studentNumber",
+     headerName: "شماره دانشجویی",
+     width: 200,
+   },
+ ];
+
   return (
-    <div>Students</div>
-  )
+    <section id="students">
+      {rows?.length && (
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+        />
+      )}
+    </section>
+  );
 }
-
-export default Students
